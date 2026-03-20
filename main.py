d36815065e551944d7e5e5b5eebc0e8012835fce
Angelo Sotgiu <angelo.sotgiu@unica.it>
2026-03-20 10:25:52 +0100

    complete lab 1

--- main.py
+++ main.py
@@ -5,5 +5,30 @@ app = FastAPI()
 
 
 @app.get("/")
-def hello_world():
-    return "Hello World!"
+def hello_world(
+    q: str,
+    sort: bool = False
+) -> dict[str, str | bool]:
+    return {"q": q, "sort": sort}
+
+
+@app.get("/home")
+def homepage():
+    return "This is the homepage"
+
+
+@app.get("/{username}")
+def username_webpage(
+    username: str
+):
+    return f"This is the webpage of user {username}."
+
+
+@app.get("/{username}/orders/{order_id}")
+def orders_webpage(
+    username: str,
+    q: str,
+    order_id: int,
+    sort: bool = False
+):
+    return f"Order {order_id} for user {username}. Q: {q}. Sorted: {sort}."
