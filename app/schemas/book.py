0b78f2d95fb83f17f4784e5c85425973bb3384ff
Angelo Sotgiu <angelo.sotgiu@unica.it>
2026-05-06 09:13:25 +0200

    update lab 4 (to complete)

--- app/schemas/book.py
+++ app/schemas/book.py
@@ -1,34 +1,20 @@
-from pydantic import BaseModel, Field
 from typing import Annotated
+from sqlmodel import SQLModel, Field
 
 
-class BookPatch(BaseModel):
-    title: str | None = None
-    author: str | None = None
-
-
-class Book(BaseModel):
-    id: int
+class BookBase(SQLModel):
     title: str
     author: str
     review: Annotated[int, Field(ge=1, le=5)] = None
 
-    model_config = {
-        "json_schema_extra": {
-            "examples": [
-                {
-                    "id": 1,
-                    "title": "Il nome della Rosa",
-                    "author": "Umberto Eco",
-                    "review": 5
-                }
-            ]
-        }
-    }
+
+class BookCreate(BookBase):
+    pass
 
 
-books = {
-    0: Book(id=0, title="Il nome della rosa", author="Umberto Eco", review=5),
-    1: Book(id=1, title="Il gioco dei sei", author="Umberto Eco", review=1),
-    2: Book(id=2, title="Il gioco dei sette", author="Maccio Capatonda", review=3),
-}
+class BookPublic(BookBase):
+    id: int
+
+
+class BookDB(BookBase, table=True):
+    id: int = Field(default=None, primary_key=True)
