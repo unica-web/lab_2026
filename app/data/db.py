020d8730c5b47162f223560c27d8092973a35db6
Angelo Sotgiu <angelo.sotgiu@unica.it>
2026-05-06 09:32:48 +0200

    update lab 4 (to complete)

--- app/data/db.py
+++ app/data/db.py
@@ -1,4 +1,6 @@
 from sqlmodel import create_engine, SQLModel, Session
+from typing import Annotated
+from fastapi import Depends
 
 
 sqlite_file_name = "/home/asotgiu/lab_2026/app/data/database.db"
@@ -9,5 +11,13 @@ engine = create_engine(
     echo=True
 )
 
+
 def init_database():
     SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
